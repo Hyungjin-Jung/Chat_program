5bb778f9e4c7b1b6fe5c842fd82299ab68f9d682
Hyungjin-Jung <1hyungjin1@naver.com>
2023-03-24 17:23:49 +0900

    새출발

--- Project1/Project1/git-test.cpp
+++ Project1/Project1/git-test.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
+
 double circle(int r, float pi)
 {
 	return (double)r * (double)r * pi;
+}
+
+using namespace std;
+
+int circle2()
+{
+	cout << "1 + 1 = 3";
+	cout << "1 + 1 = 2";
+	cout << "2 + 4 = 6";
 }
 
 int main()
@@ -10,7 +20,7 @@ int main()
 	int r;
 	double pi = 3.141592;
 	
-	std::cout << "반지름 입력: ";
+	std::cout << "Radius: ";
 	std::cin >> r;
-	std::cout << "원의 넓이는 " << circle(r, pi) << std::endl;
+	std::cout << "Area: " << circle(r, pi) << std::endl;
 }
